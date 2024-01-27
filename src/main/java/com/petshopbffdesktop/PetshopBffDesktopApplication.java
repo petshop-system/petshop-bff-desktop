2bd8777c65e44c3195aee6dd4c79864577601251
MaironCosta <maironmendes@yahoo.com.br>
2024-01-27 11:03:59 -0300

    initial commit

---
+++ src/main/java/com/petshopbffdesktop/PetshopBffDesktopApplication.java
@@ -0,0 +1,65 @@
+package com.petshopbffdesktop;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootApplication
+public class PetshopBffDesktopApplication implements CommandLineRunner {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Value(value = "${spring.config.activate.on-profile}")
+	private String activeProfile;
+
+	@Value(value = "${redis.host}")
+	private String redisHost;
+
+	@Value(value = "${redis.port}")
+	private int redisPort;
+
+	public static void main(String[] args) {
+		SpringApplication app = new SpringApplication(PetshopBffDesktopApplication.class);
+		app.run();
+	}
+
+	@Bean
+	public ObjectMapper objectMapper() {
+		ObjectMapper objectMapper = new ObjectMapper();
+		return objectMapper.registerModule(new JavaTimeModule());
+	}
+
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory() {
+		RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new JedisConnectionFactory(config);
+	}
+
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate() {
+		RedisTemplate<String, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(jedisConnectionFactory());
+		return template;
+	}
+
+	@Override
+	public void run(String... args) {
+
+		logger.info("#############################################################");
+		logger.info("active profile: {}", activeProfile);
+		logger.info("redisHost: {}", redisHost);
+		logger.info("redisPort: {}", redisPort);
+		logger.info("#############################################################");
+
+	}
+
+}
---
+++ src/test/java/com/petshopbffdesktop/PetshopBffDesktopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petshopbffdesktop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetshopBffDesktopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
